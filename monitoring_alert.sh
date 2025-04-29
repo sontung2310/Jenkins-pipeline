ef28274dde73b0af9ab2b64b09e884af727829da
sontung2310 <buisontung2310@gmail.com>
2025-04-29 23:15:26 +1000

    Monitoring update

--- monitoring_alert.sh
+++ monitoring_alert.sh
@@ -5,7 +5,6 @@ cluster_name="flaskappcluster"
 action_group_name="MonitoringandAlerting"
 action_group_short_name="maa"
 email="buisontung2310@gmail.com"
-workspace_id="<your-workspace-id>"
 
 subscription_id=$(az group show --name $resource_group --query "id" -o tsv | cut -d'/' -f3)
 
@@ -22,16 +21,18 @@ echo "Action Group ID: $action_group_id"
 # Update the Action Group to add an email receiver
 az monitor action-group update \
   --ids $action_group_id \
-  --add emailReceivers email=$email
+  --add emailReceivers name='MyEmailReceiver' emailAddress="$email"
+  
+# az monitor metrics list-definitions \
+#   --resource "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.ContainerService/managedClusters/$cluster_name" \
+#   --output table
 
-# Create an alert for HTTP 4xx or 5xx errors in FlaskApp
 az monitor metrics alert create \
-  --name "FlaskAppErrorAlert" \
-  --resource-group $resource_group \
+  --name "AKSHighCPUAlert" \
+  --resource-group "$resource_group" \
   --scopes "/subscriptions/$subscription_id/resourceGroups/$resource_group/providers/Microsoft.ContainerService/managedClusters/$cluster_name" \
-  --description "Alert when Flask API returns 4xx or 5xx errors" \
-  --condition "count AzureDiagnostics | where StatusCode >= 400 and StatusCode < 600 > 0" \
-  --action-group $action_group_id \
-  --enabled true \
-  --frequency "PT5M" \
-  --window-size "PT15M"
+  --description "Alert if average CPU > 80% for 15 mins" \
+  --condition "avg apiserver_cpu_usage_percentage > 80" \
+  --window-size "PT5M" \
+  --evaluation-frequency "PT5M" \
+  --action "$action_group_id"
